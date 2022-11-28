5e74cec6fcd598294ef3c40245f390b3499eb1ce
Nihad <https://github.com/nshukur>
2022-11-28 01:58:16 -0500

    create web app for mining

--- Create Blockchain/blockchain.py
+++ Create Blockchain/blockchain.py
@@ -1,5 +1,6 @@
-# Module 1 - Create blockchain
+# Module 1 - Create a Blockchain
 
+# Importing the libraries
 import datetime
 import hashlib
 import json
@@ -54,3 +55,37 @@ class Blockchain:
             previous_block = block
             block_index += 1
         return True
+
+# Part 2 - Mining our Blockchain
+
+# Creating a Web App
+app = Flask(__name__)
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = False
+
+# Creating a Blockchain
+blockchain = Blockchain()
+
+# Mining a new block
+@app.route('/mine_block', methods = ['GET'])
+def mine_block():
+    previous_block = blockchain.get_previous_block()
+    previous_proof = previous_block['proof']
+    proof = blockchain.proof_of_work(previous_proof)
+    previous_hash = blockchain.hash(previous_block)
+    block = blockchain.create_block(proof, previous_hash)
+    response = {'message': 'Congratulations, you just mined a block!',
+                'index': block['index'],
+                'timestamp': block['timestamp'],
+                'proof': block['proof'],
+                'previous_hash': block['previous_hash']}
+    return jsonify(response), 200
+
+# Getting the full Blockchain
+@app.route('/get_chain', methods = ['GET'])
+def get_chain():
+    response = {'chain': blockchain.chain,
+                'length': len(blockchain.chain)}
+    return jsonify(response), 200
+
+# Running the app
+app.run(host = '0.0.0.0', port = 5001)
